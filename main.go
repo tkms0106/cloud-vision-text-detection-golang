2f0d88267ed9ace883ea4999225b59ed14b5dfe9
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-03-08 14:14:29 +0900

    [add]検出したテキストをログに出力
    https://pkg.go.dev/cloud.google.com/go/vision/apiv1

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"crypto/sha1"
+	"errors"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -10,6 +13,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/mattn/go-colorable"
 	"github.com/olahol/go-imageupload"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+
+	vision "cloud.google.com/go/vision/apiv1"
 )
 
 func main() {
@@ -18,23 +25,70 @@ func main() {
 
 	r.Use(static.Serve("/", static.LocalFile("./assets", true)))
 
-	r.POST("/upload", func(c *gin.Context) {
-		img, err := imageupload.Process(c.Request, "file")
-		if err != nil {
-			panic(err)
-		}
-		thumb, err := imageupload.ThumbnailPNG(img, 300, 300)
-		if err != nil {
-			panic(err)
-		}
-		h := sha1.Sum(thumb.Data)
-		thumb.Save(fmt.Sprintf("files/%s_%x.png",
-			time.Now().Format("20060102150405"), h[:4]))
-	})
+	r.POST("/upload", uploadHandler)
 
 	port, ok := os.LookupEnv("PORT")
 	if !ok {
 		port = "5000"
 	}
 	r.Run(":" + port)
+}
+
+func uploadHandler(c *gin.Context) {
+	img, err := imageupload.Process(c.Request, "file")
+	if err != nil {
+		panic(err)
+	}
+	thumb, err := imageupload.ThumbnailPNG(img, 300, 300)
+	if err != nil {
+		panic(err)
+	}
+	h := sha1.Sum(thumb.Data)
+	filepath := fmt.Sprintf(
+		"%s_%x.png",
+		time.Now().Format("20060102150405"), h[:4],
+	)
+	err = thumb.Save(filepath)
+	if err != nil {
+		log.Fatalf("Failed to save file: %v", err)
+	}
+	detectDocumentText(filepath)
+	os.Remove(filepath)
+}
+
+func detectDocumentText(filepath string) error {
+	client, ctx := generateClient()
+	defer client.Close()
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+	defer file.Close()
+	image, err := vision.NewImageFromReader(file)
+	if err != nil {
+		log.Fatalf("Failed to create image: %v", err)
+	}
+	text, err := client.DetectDocumentText(ctx, image, nil)
+	log.Println(text.GetText())
+	os.Remove(filepath)
+	return nil
+}
+
+func generateClient() (*vision.ImageAnnotatorClient, context.Context) {
+	json, ok := os.LookupEnv("GOOGLE_APPLICATION_CREDENTIALS")
+	if !ok {
+		log.Fatalln("Environment variable GOOGLE_APPLICATION_CREDENTIALS was not found.")
+	}
+	jwtConfig, err := google.JWTConfigFromJSON([]byte(json), vision.DefaultAuthScopes()...)
+	if err != nil {
+		panic(errors.New("google.JWTConfigFromJSON :" + err.Error() + "\n" + json))
+	}
+	ctx := context.Background()
+	ts := jwtConfig.TokenSource(ctx)
+	client, err := vision.NewImageAnnotatorClient(ctx, option.WithTokenSource(ts))
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	return client, ctx
 }
