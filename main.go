a7d2b92e3938a2f5a79465dcbad5e63845b50d33
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-03-11 23:33:48 +0900

    [update]clientの生成タイミングをmain先頭に変更
    外からclientを渡すためにuploadHandlerFuncを作り、gin.HandlerFuncを返させる
    画像ファイルの処理周りでエラーの場合panicに統一
    client生成中のエラーはlog.Fatalfに統一

--- main.go
+++ main.go
@@ -20,79 +20,91 @@ import (
 )
 
 func main() {
+	client := generateClient()
+	defer client.Close()
+
 	gin.DefaultWriter = colorable.NewColorableStdout()
 	r := gin.Default()
-
 	r.Use(static.Serve("/", static.LocalFile("./assets", true)))
-
-	r.POST("/upload", uploadHandler)
+	r.POST("/upload", uploadHandlerFunc(client))
 
 	port, ok := os.LookupEnv("PORT")
 	if !ok {
 		port = "5000"
 	}
+
 	err := r.Run(":" + port)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func uploadHandler(c *gin.Context) {
-	img, err := imageupload.Process(c.Request, "file")
-	if err != nil {
-		panic(err)
+func uploadHandlerFunc(client *vision.ImageAnnotatorClient) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		img, err := imageupload.Process(c.Request, "file")
+		if err != nil {
+			panic(err)
+		}
+		thumb, err := imageupload.ThumbnailPNG(img, 300, 300)
+		if err != nil {
+			panic(err)
+		}
+
+		h := sha1.Sum(thumb.Data)
+
+		filepath := fmt.Sprintf(
+			"%s_%x.png",
+			time.Now().Format("20060102150405"), h[:4],
+		)
+		err = thumb.Save(filepath)
+		if err != nil {
+			panic(err)
+		}
+
+		detectDocumentText(client, filepath)
+		os.Remove(filepath)
 	}
-	thumb, err := imageupload.ThumbnailPNG(img, 300, 300)
-	if err != nil {
-		panic(err)
-	}
-	h := sha1.Sum(thumb.Data)
-	filepath := fmt.Sprintf(
-		"%s_%x.png",
-		time.Now().Format("20060102150405"), h[:4],
-	)
-	err = thumb.Save(filepath)
-	if err != nil {
-		log.Fatalf("Failed to save file: %v", err)
-	}
-	detectDocumentText(filepath)
-	os.Remove(filepath)
 }
 
-func detectDocumentText(filepath string) {
-	client, ctx := generateClient()
-	defer client.Close()
-
+func detectDocumentText(client *vision.ImageAnnotatorClient, filepath string) string {
 	file, err := os.Open(filepath)
 	if err != nil {
 		log.Fatalf("Failed to read file: %v", err)
 	}
 	defer file.Close()
+
 	image, err := vision.NewImageFromReader(file)
 	if err != nil {
 		log.Fatalf("Failed to create image: %v", err)
 	}
+
+	ctx := context.Background()
 	text, err := client.DetectDocumentText(ctx, image, nil)
 	if err != nil {
 		log.Fatalf("Failed to detect document text: %v", err)
 	}
+
 	log.Println(text.GetText())
+	return text.GetText()
 }
 
-func generateClient() (*vision.ImageAnnotatorClient, context.Context) {
+func generateClient() *vision.ImageAnnotatorClient {
 	json, ok := os.LookupEnv("GOOGLE_APPLICATION_CREDENTIALS")
 	if !ok {
-		log.Fatalln("Environment variable GOOGLE_APPLICATION_CREDENTIALS was not found.")
+		log.Fatalf("Environment variable GOOGLE_APPLICATION_CREDENTIALS was not found.")
 	}
+
 	jwtConfig, err := google.JWTConfigFromJSON([]byte(json), vision.DefaultAuthScopes()...)
 	if err != nil {
-		panic(errors.New("google.JWTConfigFromJSON :" + err.Error() + "\n" + json))
+		log.Fatalf("%v", errors.New("google.JWTConfigFromJSON :"+err.Error()+"\n"+json))
 	}
+
 	ctx := context.Background()
 	ts := jwtConfig.TokenSource(ctx)
 	client, err := vision.NewImageAnnotatorClient(ctx, option.WithTokenSource(ts))
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
-	return client, ctx
+
+	return client
 }
